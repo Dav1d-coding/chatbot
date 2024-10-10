d93f3889339a801ff6acdc34e61148f3ae9d06e9
David <75938819+Dav1d-coding@users.noreply.github.com>
2024-10-10 14:25:57 +0000

    hel

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,22 +7,36 @@ import torch
 st.title("💬 Chatbot")
 st.write("Hello")
 
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
-)
+MAX_REASONING_TOKENS = 1024
+MAX_RESPONSE_TOKENS = 512
 
+model_name = "KingNish/Reasoning-Llama-1b-v0.1"
+
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "Which is greater 9.9 or 9.11 ??"
 messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    {"role": "user", "content": "Who are you?"},
+    {"role": "user", "content": prompt}
 ]
 
-outputs = pipeline(
-    messages,
-    max_new_tokens=256,
-)
-st.write(outputs[0]["generated_text"][-1])
+# Generate reasoning
+reasoning_template = tokenizer.apply_chat_template(messages, tokenize=False, add_reasoning_prompt=True)
+reasoning_inputs = tokenizer(reasoning_template, return_tensors="pt").to(model.device)
+reasoning_ids = model.generate(**reasoning_inputs, max_new_tokens=MAX_REASONING_TOKENS)
+reasoning_output = tokenizer.decode(reasoning_ids[0, reasoning_inputs.input_ids.shape[1]:], skip_special_tokens=True)
+
+# print("REASONING: " + reasoning_output)
+
+# Generate answer
+messages.append({"role": "reasoning", "content": reasoning_output})
+response_template = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+response_inputs = tokenizer(response_template, return_tensors="pt").to(model.device)
+response_ids = model.generate(**response_inputs, max_new_tokens=MAX_RESPONSE_TOKENS)
+response_output = tokenizer.decode(response_ids[0, response_inputs.input_ids.shape[1]:], skip_special_tokens=True)
+
+
+st.write(response_output)
+st.write("exit")
